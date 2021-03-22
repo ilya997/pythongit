f57c585684de908eb0ddc8fbc5f5bc621b1ecc78
ilya997 <akhtyamov_ilya@mail.ru>
2021-03-22 11:55:11 +0500

    git init

--- amaterasu.py
+++ amaterasu.py
@@ -1,19 +1,25 @@
 
-films = ['Крепкий орешек', 'Назад в будущее', 'Таксист', 'Леон', 'Богемская рапсодия', 'Город грехов', 'Мементо', 'Отступники', 'Деревня']
-like_films = []
-while True:
-    search = input('Какой фильм ищем? ')
+old_list = []
+new_list = []
+i2 = 0
+n = int(input('Введите кол-во чисел: '))
+for _ in range(n):
+    number = int(input('Введите число: '))
+    old_list.append(number)
+    new_list.append(0)
 
 
+k = int(input('Введите сдвиг: '))
+if k > n:
+     while k > n:
+         k -= n
+for i2 in range(n):
+    if i2 + k >= n:
+       new_list[i2] = old_list[(i2 + k)- n]
+    else:
+        new_list[i2] = old_list[i2 + k]
 
-
-
-
-
-
-
-
-
+print(new_list)
 
 
 
